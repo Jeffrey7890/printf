848ae619c3a7fde67f578ff191a821cd84fc6756
Jeffrey7890 <jeffrey.ogechi@gmail.com>
2024-02-02 13:07:51 +0100

    numbers

---
+++ _s_write_line.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _s_write_line - write a string
+ * @s: string to write
+ * Return: length of string
+ */
+int _s_write_line(const char *s)
+{
+	int cnt = 0, ascii_num;
+
+	while (*s != '\0')
+	{
+		ascii_num = *s;
+		if ((0 < ascii_num && ascii_num < 32) || ascii_num >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			if (ascii_num < 16)
+				_putchar('0');
+			_printnum(ascii_num, 16);
+		}
+		else
+			_putchar(*s);
+		cnt++;
+		s++;
+	}
+	return (cnt);
+}
--- main.h
+++ main.h
@@ -5,6 +5,7 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _write_line(char *s);
 int _printnum(int numb, int base);
+int _s_write_line(const char *s);
 int _printbin(int numb);
 
 #endif
